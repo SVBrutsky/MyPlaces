7becbc42a7062259bbd1aff7f9ccde3904a7c13b
Sergey Brutsky <svbrutsky@icloud.com>
2022-09-10 14:07:45 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Sergey Brutsky on 10.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
